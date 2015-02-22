35d71a548ecdbf13ef0ef07c5871333ebcd8051a
Erin Westfall <erinkrlwestfall@gmail.com>
2015-02-22 14:19:08 -0500

    adding create in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,19 @@ class SessionsController < ApplicationController
 	  end
 
 	  def create
+	  	@user = User.new(user_params)
+	  		if @user.save
+	  			sesion[:user_id] = @user.user_id
+	  			redirect_to('/about')
+	  		else
+	  			redirect_to('/login')
+	  		end
 	  end
+
+def destroy
+	session[:user_id] = nill
+	redirect_to('/')
+end
 
 
 end
